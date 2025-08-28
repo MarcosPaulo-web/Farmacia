c3dc4b02b748c77fbf690d03b852b628f643bcb7
MarcosPaulo-web <marcos_p_medeiros@estudante.sesisenai.org.br>
2025-08-28 15:33:31 -0300

    Feature: Criação do hello word e inicio da api farmacia

---
+++ marcos/src/main/java/com/farmacia/marcos/model/Produtos.java
@@ -0,0 +1,28 @@
+package com.farmacia.marcos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name = "TBPRODUTOS")
+public class Produtos {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cdProduto")// para indentificar que é id
+    private Integer cdProduto;
+
+    @Column(name = "nmProduto")
+    private String nmProduto;
+
+    @Column(name = "dsBula")
+    private String dsBula;
+
+    @Column(name = "vlProduto")
+    private double vlProduto;
+
+}
